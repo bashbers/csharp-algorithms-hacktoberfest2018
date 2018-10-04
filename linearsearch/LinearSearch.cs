3a8a95716eee98dc9f27834f2346583b91c6aba8
Gopesh Sharma <gopesh_sharma@gensler.com>
2018-10-04 17:55:34 +0530

    Added Linear Search

---
+++ linearsearch/LinearSearch.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using csharp_algorithms;
+
+namespace linearsearch
+{
+    public class LinearSearch
+    {
+        private readonly IEnumerable<Node> _list;
+        public LinearSearch(IEnumerable<Node> list)
+        {
+            this._list = list;
+        }
+
+        public bool Search(Node value)
+        {
+            for (int i = 0; i < _list.Count(); i++)
+            {
+                Node currentValue = _list.ElementAt(i);
+                if (currentValue.Number == value.Number)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
