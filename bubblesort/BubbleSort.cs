bf295b767dfff912f08a97bf095e8470ab326789
suvo696 <43770687+suvo696@users.noreply.github.com>
2018-10-04 19:11:12 +0530

    u didnt have any main program

--- bubblesort/BubbleSort.cs
+++ bubblesort/BubbleSort.cs
@@ -1,56 +1,33 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using csharp_algorithms;
-
-namespace bubblesort
+/*
+ * C# Program to Perform Bubble Sort
+ */
+using System;
+class bubblesort
 {
-    public class BubbleSort
-    {
-        private readonly IEnumerable<Node> _list;
-        public BubbleSort(IEnumerable<Node> list)
+        static void Main(string[] args)
         {
-            this._list = list;
-        }
-
-        private void Swap(Node left, Node right)
-        {
-            int originalLeft = left.Number;
-            left.Number = right.Number;
-            right.Number = originalLeft;
-        }
-
-        public IEnumerable<Node> Sort()
-        {
-            for (int outer = 0; outer < _list.Count() -1; outer++)
+            int[] a = { 3, 2, 5, 4, 1 };  
+            int t;
+            Console.WriteLine("The Array is : ");
+            for (int i = 0; i < a.Length; i++)
             {
-                for (int inner = 0; inner < _list.Count() - outer -1; inner++)
+                Console.WriteLine(a[i]);
+            }
+            for (int j = 0; j <= a.Length - 2; j++)
+            {
+                for (int i = 0; i <= a.Length - 2; i++)
                 {
-                    Node current = _list.ElementAt(inner);
-                    Node next = _list.ElementAt(inner + 1);
-                    if (current.Number > next.Number)
+                    if (a[i] > a[i + 1])
                     {
-                        Swap(current, next);
+                        t = a[i + 1];
+                        a[i + 1] = a[i];
+                        a[i] = t;
                     }
                 }
             }
-            return _list;
-        }
-
-        public IEnumerable<Node> ReverseSort()
-        {
-            for (int outer = _list.Count() -1; outer >= 0; outer--)
-            {
-                for (int inner = _list.Count() - 1; inner > 0; inner--)
-                {
-                    Node current = _list.ElementAt(inner);
-                    Node next = _list.ElementAt(inner - 1);
-                    if (current.Number < next.Number)
-                    {
-                        Swap(current, next);
-                    }
-                }
-            }
-            return _list;
+            Console.WriteLine("The Sorted Array :");
+            foreach (int aray in a)                         
+                Console.Write(aray + " ");
+            Console.ReadLine();
         }
     }
-}
