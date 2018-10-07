30769f527f14acd2eabd0e07248eaa07c7e848ab
pkos98 <patrick.kostas@hs-augsburg.de>
2018-10-07 18:52:06 +0200

    Implement bogosort

---
+++ Tests/sort/BogoSortTests.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using bogosort;
+using csharp_algorithms;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests.sort
+{
+    [TestClass]
+    public class BogoSortTests
+    {
+        [TestMethod]
+        public void NormalSort()
+        {
+            //Arrange
+            List<Node> list = new List<Node>
+            {
+                new Node(5),
+                new Node(1)
+            };
+            BogoSort b = new BogoSort(list);
+            //Act
+            List<Node> result = b.Sort().ToList();
+            //Assert
+            Assert.AreEqual(1, result.ElementAt(0).Number);
+            Assert.AreEqual(5, result.ElementAt(1).Number);
+        }
+        [TestMethod]
+        public void ReverseSort()
+        {
+            //Arrange
+            List<Node> list = new List<Node>
+            {
+                new Node(5),
+                new Node(1)
+            };
+            var b = new BogoSort(list);
+            //Act
+            List<Node> result = b.ReverseSort().ToList();
+            //Assert
+            Assert.AreEqual(1, result.ElementAt(0).Number);
+            Assert.AreEqual(5, result.ElementAt(1).Number);
+        }
+    }
+}
---
+++ bogosort/BogoSort.cs
@@ -0,0 +1,59 @@
+﻿using csharp_algorithms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace bogosort
+{
+    public class BogoSort
+    {
+        private readonly IEnumerable<Node> _list;
+        public BogoSort(IEnumerable<Node> list)
+        {
+            this._list = list;
+        }
+   
+        public IEnumerable<Node> Sort()
+        {
+            Random random = new Random();
+            while (!IsSorted())
+            {
+                int a = random.Next(_list.Count());
+                int b = random.Next(_list.Count());
+                Swap(_list.ElementAt(a), _list.ElementAt(b));
+            }
+            return _list;
+        }
+
+        public IEnumerable<Node> ReverseSort()
+        {
+            Random random = new Random();
+            while (!IsSorted())
+            {
+                int a = random.Next(_list.Count());
+                int b = random.Next(_list.Count());
+                Swap(_list.ElementAt(b), _list.ElementAt(a));
+            }
+            return _list;
+        }
+
+        private void Swap(Node left, Node right)
+        {
+            int originalLeft = left.Number;
+            left.Number = right.Number;
+            right.Number = originalLeft;
+        }
+
+        private bool IsSorted()
+        {
+            for (int i = 0; i < _list.Count(); i++)
+            {
+                Node currentNode = _list.ElementAt(i);
+                for (int j = i+1; j < _list.Count(); j++)
+                    if (currentNode.Number > _list.ElementAt(j).Number)
+                        return false;
+            }
+            return true;
+        }
+    }
+}
