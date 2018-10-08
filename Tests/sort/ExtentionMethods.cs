cc8c59de159f5133465e280402fa9f623d1b276c
Daniel P <dpisanu@users.noreply.github.com>
2018-10-08 21:03:29 +0200

    Adding sortings via LINQ
    Issue: #2
    
      Have you followed the guidelines in our Readme.md document?
      Have you checked to ensure there aren't other open Pull Requests for the same update/change?
      Have you created tests for your algorithm?
    If you can check all of the above, congrats! ✨
    
    Description:
    Sorting done via LINQ.
    GroupBy
    Sort with a generic and a type converter.

---
+++ Tests/sort/ExtentionMethods.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using csharp_algorithms;
+
+namespace Tests.sort
+{
+    internal static class ExtentionMethods
+    {
+        public static Node[] Copy(this Node[] array)
+        {
+            Node[] arrayCopy = new Node[array.Length];
+            Array.Copy(array, arrayCopy, array.Length);
+            return arrayCopy;
+        }
+                
+        public static bool AreSameOrder(this Node[] a, Node[] b)
+        {
+            if (a == null|| b == null) return false;
+            if (a.Count() != b.Count()) return false;
+
+            foreach(var node in a)
+            {
+                var indexOfNode = Array.IndexOf(a, node);
+                if (b[indexOfNode].Number == node.Number)
+                {
+                    continue;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return a.Count() == b.Count();
+        }
+    }
+}
---
+++ Tests/sort/TestDataGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using csharp_algorithms;
+
+namespace Tests.sort
+{
+    class TestDataGenerator
+    {
+        private readonly int Min = int.MinValue;
+        private readonly int Max = int.MaxValue;
+        private readonly Random Random = new Random();
+
+        public Node[] Generate(int entries = 256)
+        {
+            if (entries <= 2)
+            {
+                return new Node[0];
+            }
+
+            var data = Enumerable
+                .Repeat(0, entries)
+                .Select(i => new Node(Random.Next(Min, Max)))
+                .ToArray();
+
+            return data;
+        }        
+    }
+}
