c57224f75ea7a2611bb88f88e3145a857909d19d
Masayuki Takeda <rexiaxm7@gmail.com>
2018-10-11 06:54:28 +0900

    Add Knapsack Problem

---
+++ knapsackproblem/KnapsackProblem.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace knapsackproblem
+{
+    public static class KnapsackProblem
+    {
+        public static HashSet<Thing> Resolve(IEnumerable<Thing> things, int maxWeight)
+        {
+            var orderedThings = things
+                .OrderByDescending(thing => thing.EvaluationValue)
+                .ThenByDescending(thing => thing.Weight)
+                .ToArray();
+
+            var currentWeight = 0;
+            var result = new HashSet<Thing>();
+
+            foreach (var thing in orderedThings)
+            {
+                if (maxWeight - currentWeight < thing.Weight) continue;
+
+                currentWeight += thing.Weight;
+                result.Add(thing);
+                if (maxWeight - currentWeight == 0) break;
+            }
+
+            return result;
+        }
+    }
+}
