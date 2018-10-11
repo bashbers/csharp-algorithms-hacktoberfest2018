00cd58f9786d9465eb46ddf43c39309817b04b5a
Maros <445327@mail.muni.cz>
2018-10-11 22:57:34 +0200

    Implemented Bellman-Ford algorithm and added Graph class with other neccessary classes.

---
+++ bellmanFordAlgorithm/BellmanFordAlgorithm.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Path = System.Collections.Generic.List<bellmanFordAlgorithm.Graph.Edge>;
+
+namespace bellmanFordAlgorithm
+{
+    /// <summary>
+    /// Algorithm that computes shortest paths from a single source
+    /// vertex to all of the other vertices in a weighted digraph.
+    /// </summary>
+    public class BellmanFordAlgorithm
+    {
+        #region Nested Classes
+        /// <summary>
+        /// Shortest paths in a weighted digraph;
+        /// </summary>
+        public class ShortestPaths
+        {
+            /// <summary>
+            /// Shortest distances from the source vertex for each of the vertices.
+            /// </summary>
+            public IDictionary<string, int> Distances
+            {
+                get;
+                private set;
+            }
+
+            /// <summary>
+            /// Shortes paths from the source vertex for each of the vertices.
+            /// </summary>
+            public IDictionary<string, Path> Paths
+            {
+                get;
+                private set;
+            }
+
+            /// <summary>
+            /// Creates an (empty) instance of the ShortestPaths.
+            /// </summary>
+            /// <param name="distances"></param>
+            /// <param name="paths"></param>
+            public ShortestPaths(IDictionary<string, int> distances, IDictionary<string, Path> paths)
+            {
+                Distances = distances;
+                Paths = paths;
+            }
+        } 
+        #endregion // Nested Classes
+        
+        /// <summary>
+        /// Represents logical infinity - distance to undiscovered vertices.
+        /// </summary>
+        public const int Inf = int.MaxValue;
+
+        #region Methods
+        /// <summary>
+        /// Computes shortest paths from source vertex to each of the other vertices.
+        /// </summary>
+        /// <param name="graph"></param>
+        /// <param name="detectNegativeCycles"></param>
+        /// <returns></returns>
+        public ShortestPaths Compute(Graph graph, bool detectNegativeCycles = false)
+        {
+            var g = graph;
+            var distances = new Dictionary<string, int>();
+            var paths = new Dictionary<string, Path>();
+
+            foreach (var v in g.Vertices)
+            {
+                distances[v] = Inf;
+            }
+            distances[g.Source] = 0;
+
+            for (int i = 0; i < g.Vertices.Count - 1; i++)
+            {
+                foreach (var edge in g.Edges)
+                {
+                    var tmpDistance = distances[edge.Vertex1] + edge.Weight;
+
+                    if (tmpDistance < distances[edge.Vertex2])
+                    {
+                        distances[edge.Vertex2] = tmpDistance;
+                        paths[edge.Vertex2].Add(edge);
+                    }
+                }
+            }
+
+            if (detectNegativeCycles)
+            {
+                foreach (var edge in g.Edges)
+                {
+                    if (distances[edge.Vertex1] + edge.Weight < distances[edge.Vertex2])
+                        throw new ArgumentException("Graph contains a negative-weight cycle");
+                }
+            }
+
+            return new ShortestPaths(distances, paths);
+        } 
+        #endregion // Methods
+    }
+}
---
+++ bellmanFordAlgorithm/Graph.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+
+namespace bellmanFordAlgorithm
+{
+    /// <summary>
+    /// Weighted oriented graph allowing for negative edge weights.
+    /// </summary>
+    public class Graph
+    {
+        #region Nested Classes
+        /// <summary>
+        /// Edge in a graph.
+        /// </summary>
+        public class Edge
+        {
+            /// <summary>
+            /// Origin vertex.
+            /// </summary>
+            public string Vertex1;
+
+            /// <summary>
+            /// Destination vertex;
+            /// </summary>
+            public string Vertex2;
+
+            /// <summary>
+            /// Weight of the edge. Can be both positive and negative.
+            /// </summary>
+            public int Weight;
+        }
+        #endregion // Nested Classes
+
+        #region Properties
+        /// <summary>
+        /// Set of vertices of the graph.
+        /// </summary>
+        public ISet<string> Vertices
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Set of edges of the graph.
+        /// </summary>
+        public ISet<Edge> Edges
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Source vertex of the graph.
+        /// </summary>
+        public string Source
+        {
+            get;
+            private set;
+        }
+        #endregion // Properties
+
+        #region Constructors
+        /// <summary>
+        /// Creates new instance of the graph.
+        /// </summary>
+        /// <param name="vertices"></param>
+        /// <param name="edges"></param>
+        /// <param name="source"></param>
+        public Graph(ISet<string> vertices, ISet<Edge> edges, string source)
+        {
+            Vertices = vertices;
+            Edges = edges;
+            Source = source;
+        }
+        #endregion // Constructors
+    }
+}
