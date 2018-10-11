a02b56f4da5bbb721a7cf7c8b06824f68a31adb3
Gabriel <camed.tf@gmail.com>
2018-10-11 20:31:22 +0200

    ###2 insertion sort
    * [*] Have you followed the guidelines in our Readme.md document?
    * [*] Have you checked to ensure there aren't other open [Pull Requests](../../pulls) for the same update/change?
    * [*] Have you created tests for your algorithm?
    
    ### Description:
    Unefficient sorting algorithm with time complexity O(n^2). Best case scenario: Everything is ordered at the start, complexity then is O(n).
    Worst case scenario:
    Everything is disordered at the start, complexity then is O(n^2)

---
+++ Tests/sort/InsertionsortTests.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using csharp_algorithms;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using insertionsort;
+
+namespace Tests.sort
+{
+    [TestClass]
+    public class InsertionsortTests
+    {
+        [TestMethod]
+        public void NormalSort()
+        {
+            //Arrange
+            List<Node> list = new List<Node>
+            {
+                new Node(5),
+                new Node(1)
+            };
+            InsertionSort s = new InsertionSort(list);
+
+            //Act
+            List<Node> result = s.Sort().ToList();
+
+            //Assert
+            Assert.AreEqual(1, result.ElementAt(0).Number);
+            Assert.AreEqual(5, result.ElementAt(1).Number);
+        }
+
+        [TestMethod]
+        public void ReverseSort()
+        {
+            //Arrange
+            List<Node> list = new List<Node>
+            {
+                new Node(5),
+                new Node(1)
+            };
+            SelectionSort s = new SelectionSort(list);
+
+            //Act
+            List<Node> result = s.ReverseSort().ToList();
+
+            //Assert
+            Assert.AreEqual(1, result.ElementAt(0).Number);
+            Assert.AreEqual(5, result.ElementAt(1).Number);
+
+        }
+    }
+}
---
+++ insertionsort/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("insertionsort")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("insertionsort")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("56f3d2ae-8b47-400b-abf3-ed53b6955bb0")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Rewizja
+//
+// Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
+// przy użyciu symbolu „*”, tak jak pokazano poniżej:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
