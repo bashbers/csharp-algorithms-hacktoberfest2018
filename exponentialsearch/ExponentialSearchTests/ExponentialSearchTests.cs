8b29e41c2319d6747c453ef17d020d28faf344e2
Tereza Stoyanova <stoyanova.tereza@gmail.com>
2018-10-17 14:41:21 -0400

    Added Exponential Search and Tests

---
+++ exponentialsearch/ExponentialSearchTests/ExponentialSearchTests.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+//using csharp_algorithms;
+using exponentialsearch;
+
+namespace Tests.search
+{
+    [TestClass]
+    public class ExponentialSearchTest
+    {
+        [TestMethod]
+        public void Search()
+        {
+            // Setup
+            int[] arr = { 0, 1, 10, 13, 16, 20, 22, 28, 31, 39, 45, 55 };
+            int searchElem = 10;
+
+
+            // Search
+            var result1 = ExponentialSearch.Search(arr, searchElem);
+            searchElem = 11;
+            var result2 = ExponentialSearch.Search(arr, searchElem); ;
+
+            // Assert
+            Assert.AreEqual(true, result1);
+            Assert.AreEqual(false, result2);
+        }
+    }
+}
