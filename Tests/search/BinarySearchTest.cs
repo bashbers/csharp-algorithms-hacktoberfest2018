8e011d20e9e028206db81f1973cc00d2bc0d783f
Unknown <e185150@metu.edu.tr>
2018-10-29 01:59:45 +0300

    Binary Search Algoritm with its tests

---
+++ Tests/search/BinarySearchTest.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using csharp_algorithms;
+using binarysearch;
+
+namespace Tests.search
+{
+    [TestClass]
+    public class BinarySearchTest
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            var nodes = new List<Node>
+            {
+                new Node(10),
+                new Node(5),
+                new Node(3),
+                new Node(50),
+                new Node(75),
+                new Node(81),
+                new Node(8),
+                new Node(93),
+                new Node(573),
+                new Node(111),
+                new Node(4324),
+                new Node(5532),
+                new Node(123),
+                new Node(1),
+                new Node(32),
+                new Node(64),
+                new Node(83),
+                new Node(43),
+                new Node(23),
+                new Node(131),
+                new Node(231),
+                new Node(133),
+                new Node(913),
+                new Node(1003),
+                new Node(81330),
+                new Node(4233),
+                new Node(233),
+                new Node(1033)
+            };
+
+            var bsearch = new BinarySearch(nodes);
+
+            var testValue1 = new Node(1);
+            var testValue2 = new Node(123123123);
+            var testValue3 = new Node(81330);
+
+            var indexValue1 = bsearch.Search(testValue1);
+            var indexValue2 = bsearch.Search(testValue2);
+            var indexValue3 = bsearch.Search(testValue3);
+
+            Assert.AreEqual(indexValue1,0);
+            Assert.IsNull(indexValue2);
+            Assert.AreEqual(indexValue3, nodes.Count() - 1);
+        }
+    }
+}
---
+++ binarysearch/BinarySearch.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using csharp_algorithms;
+
+namespace binarysearch
+{
+    public class BinarySearch
+    {
+        private readonly IEnumerable<Node> _list;
+
+        public BinarySearch(IEnumerable<Node> list)
+        {
+            //Ordering list in ascending order
+            _list = list.OrderBy(n => n.Number);
+        }
+
+        //This is nullable int because if we can't find the value inside the list
+        //we will return null in order to notify the user.
+        public int? Search(Node value)
+        {
+            int left = 0;
+            int right = _list.Count() - 1;
+            int mid;
+
+            //We will loop until the search space does not contain any value
+            while(left <= right)
+            {
+                //we find mid in order to compare it to Node value.
+                mid = (left + right) / 2;
+
+                //If we find our value return it's index
+                if(value.Number == _list.ElementAt(mid).Number)
+                {
+                    return mid;
+                }
+                //If our number is smaller than the mid value we're going to shrink our array by getting
+                //our right index next to mid index
+                else if(value.Number < _list.ElementAt(mid).Number)
+                {
+                    right = mid - 1;
+                }
+                //Same as previous step but since this time out value is bigger than the middle one from the array,
+                //we are going to search the upper part of the array.
+                else
+                {
+                    left = mid + 1;
+                }
+            }
+            //If we got here, we couldn't find our value inside our array.
+            return null;
+        }
+    }
+}
