f33f4bb5d650fb73b1ff65ee567ec4ab6370e970
JPaternain <jpaternain@gmail.com>
2019-10-21 19:59:25 +0200

    Add sleep sort algorithm

---
+++ Tests/sort/SleepSortTests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using sleepsort;
+
+namespace Tests.sort
+{
+    [TestClass]
+    public class SleepSortTests
+    {
+        private readonly SleepSorter _sorter;
+
+        public SleepSortTests() => _sorter = new SleepSorter();
+
+        [TestMethod]
+        public void Sort_EmptyList_EmptyListReturned()
+        {
+            var emptyList = new List<int>();
+
+            var result = _sorter.Sort(emptyList);
+
+            Assert.AreEqual(0, result.Count);
+        }
+
+        [TestMethod]
+        public void Sort_SinlgeItemList_SameListReturned()
+        {
+            var list = new List<int> { 1 };
+
+            var result = _sorter.Sort(list);
+
+            Assert.AreEqual(1, result[0]);
+        }
+
+        [TestMethod]
+        public void Sort_NegativeNumberInList_ExceptionThrown()
+        {
+            var list = new List<int> { -1, 3, 9 };
+
+            Assert.ThrowsException<ArgumentException>(() => _sorter.Sort(list));
+        }
+
+        [TestMethod]
+        public void Sort_UnsortedList_SortedListReturned()
+        {
+            var list = new List<int> { 1, 5, 2, 4, 8, 3, 12, 10 };
+            var sortedList = new List<int> { 1, 2, 3, 4, 5, 8, 10, 12 };
+
+            var result = _sorter.Sort(list);
+
+            Assert.AreEqual(sortedList[0], result[0]);
+            Assert.AreEqual(sortedList[1], result[1]);
+            Assert.AreEqual(sortedList[2], result[2]);
+            Assert.AreEqual(sortedList[3], result[3]);
+            Assert.AreEqual(sortedList[4], result[4]);
+            Assert.AreEqual(sortedList[5], result[5]);
+            Assert.AreEqual(sortedList[6], result[6]);
+            Assert.AreEqual(sortedList[7], result[7]);
+        }
+    }
+}
---
+++ sleepsort/sleepsorter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace sleepsort
+{
+    public class SleepSorter
+    {
+        public List<int> Sort(List<int> unsortedList)
+        {
+            if (unsortedList.Any(a => a < 0))
+                throw new ArgumentException("Array items must be greater than 0");
+
+            var sortedList = new List<int>();
+            var taskList = new List<Task>();
+
+            foreach (var item in unsortedList)
+            {
+                taskList.Add(Task.Factory.StartNew(() =>
+                {
+                    Thread.Sleep(1000 * item);
+
+                    sortedList.Add(item);
+                }));
+            }
+
+            Task.WaitAll(taskList.ToArray());
+
+            return sortedList;
+        }
+    }
+}
